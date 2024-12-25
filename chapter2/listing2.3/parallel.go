5cb68a8560049da249ea0bc85a0fe492c2aaf0ba
skartavykh <skartavykh@k-tech.ru>
2024-12-25 20:23:38 +0300

    TODO added

--- chapter2/listing2.3/parallel.go
+++ chapter2/listing2.3/parallel.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func doWork(id int) {
-    fmt.Printf("Work %d started at %s\n", id, time.Now().Format("15:04:05"))
-    time.Sleep(1 * time.Second)
-    fmt.Printf("Work %d finished at %s\n", id, time.Now().Format("15:04:05"))
+	fmt.Printf("Work %d started at %s\n", id, time.Now().Format("15:04:05"))
+	time.Sleep(1 * time.Second)
+	// TODO make analysis of count of goroutines. can we count right here how many of them are there in this function.
+	fmt.Printf("Work %d finished at %s\n", id, time.Now().Format("15:04:05"))
 }
 
 func main() {
-    for i := 0; i < 5; i++ {
-        go doWork(i)
-    }
-    time.Sleep(2 * time.Second)
+	for i := 0; i < 5; i++ {
+		go doWork(i)
+	}
+	time.Sleep(2 * time.Second)
 }
